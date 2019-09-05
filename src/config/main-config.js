b51f543b6a7a48190e2503371c0eba0e3795e22d
Madalina Jantea <madalina.jantea@gmail.com>
2019-09-04 20:06:34 -0700

    scaffolding and deployment

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 const path = require("path");
- const viewsFolder = path.join(__dirname, "..", "views");
- const logger = require('morgan');
+const viewsFolder = path.join(__dirname, "..", "views");
+const logger = require('morgan');
 
 module.exports = {
   init(app, express){
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
     app.use(express.static(path.join(__dirname, "..", "assets")));
-    app.use(bodyParser.urlencoded({
-      extended: true
-    }));
     app.use(logger('dev'));
   }
 };
