537e9cd1efddc4324b9bd6bcbf1513ab1ca1792d
Madalina Jantea <madalina.jantea@gmail.com>
2019-09-07 10:37:58 -0700

    user sign up

---
+++ src/db/queries.users.js
@@ -0,0 +1,38 @@
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+module.exports = {
+// #2
+  createUser(newUser, callback){
+
+// #3
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+
+// #4
+    return User.create({
+      username: newUser.username,
+      email: newUser.email,
+      password: hashedPassword
+    })
+    .then((user) => {
+      const msg = {
+        to: 'test@example.com',
+        from: 'test@example.com',
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+      sgMail.send(msg);
+      callback(null, user);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+
+}
