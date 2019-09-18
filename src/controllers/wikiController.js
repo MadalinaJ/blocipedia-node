e1dd670d4a8b0d6582c175fabc862cdacab28755
Madalina Jantea <madalina.jantea@gmail.com>
2019-09-18 12:28:21 -0700

    add markdown

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,7 +1,7 @@
 
-
 const wikiQueries = require("../db/queries.wikis.js");
 const Authorizer = require("../policies/application");
+const markdown = require( "markdown" ).markdown;
 
 module.exports = {
     index(req, res, next){
@@ -58,6 +58,7 @@ module.exports = {
             if(err || wiki == null){
                 res.redirect(404, "/");
             } else {
+                wiki.body = markdown.toHTML(wiki.body);
                 res.render("wikis/show", {wiki});
             }
         });
